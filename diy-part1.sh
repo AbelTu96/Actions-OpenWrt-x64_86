535486b51180668a67ec3f8da76fe8dfb81a21d2
Abel_Tu <47358077+AbelTu96@users.noreply.github.com>
2023-09-26 09:34:46 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,3 @@
 # Add a feed source
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-echo 'src-git istore https://github.com/linkease/istore;main' >>feeds.conf.default
